4c4192d6bdb6eced28a1a951cc11eb3a0cd66bad
kgw10 <aa01059183132@gmail.com>
2024-08-29 16:26:30 +0900

    0826

--- src/main/java/com/shop/Repository/ItemImgRepository.java
+++ src/main/java/com/shop/Repository/ItemImgRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Repository
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
-
     static List<ItemImg> findByItemIdOrderByIdAsc(Long id) {
     }
 
